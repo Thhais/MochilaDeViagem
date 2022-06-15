f01cd6d99b16d231ae4bc86822087fc321899bd0
Thais Faria <thhafaria@gmail.com>
2022-06-15 19:57:05 -0300

    js refatorado, add no lS após o evento submit, itens já preenchidos exibidos na tela

--- js/main.js
+++ js/main.js
@@ -3,36 +3,42 @@ const lista = document.querySelector("#lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 
+
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements["nome"]
     const quantidade = evento.target.elements["quantidade"]
 
-    PreencheLista(nome.value,quantidade.value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value,
+    }
+    itens.push(itemAtual)
+    localStorage.setItem("itens", JSON.stringify(itens))
+    
+    PreencheLista(itemAtual)
+
+
     nome.value = ""
     quantidade.value = ""
    
 })
  
-function PreencheLista(nome,quantidade){
+function PreencheLista(itemAtual){
     const li = document.createElement('li')
     li.classList.add("item")
     // li.innerHTML = `<strong>${quantidade}</strong> ${nome}`
 
     const strong = document.createElement('strong')
-    strong.innerHTML = quantidade
+    strong.innerHTML = itemAtual.quantidade
     //li.innerHTML = strong + nome ----> o problema está aqui, colocar um elemento dentro de outro com innerHTML cria  um objeto, não podemos criar como se fosse algo simples de html
     li.appendChild(strong)
-    li.innerHTML += nome
+    li.innerHTML += itemAtual.nome
     lista.appendChild(li)
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade,
-    }
-    itens.push(itemAtual)
-    localStorage.setItem("itens", JSON.stringify(itens))
-    
-
 }
+
+itens.forEach((elemento) => {
+    PreencheLista(elemento)
+})
