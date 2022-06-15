59dafbb17dffd7b0356671049aed8d1fe5f1a21a
Thais Faria <thhafaria@gmail.com>
2022-06-15 17:23:52 -0300

    lS recebendo a array e itens pegando o q/ tem no LS

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#novoItem")
+const lista = document.querySelector("#lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements["nome"]
+    const quantidade = evento.target.elements["quantidade"]
+
+    PreencheLista(nome.value,quantidade.value)
+    nome.value = ""
+    quantidade.value = ""
+   
+})
+ 
+function PreencheLista(nome,quantidade){
+    const li = document.createElement('li')
+    li.classList.add("item")
+    // li.innerHTML = `<strong>${quantidade}</strong> ${nome}`
+
+    const strong = document.createElement('strong')
+    strong.innerHTML = quantidade
+    //li.innerHTML = strong + nome ----> o problema está aqui, colocar um elemento dentro de outro com innerHTML cria  um objeto, não podemos criar como se fosse algo simples de html
+    li.appendChild(strong)
+    li.innerHTML += nome
+    lista.appendChild(li)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade,
+    }
+    itens.push(itemAtual)
+    localStorage.setItem("itens", JSON.stringify(itens))
+    
+
+}
